c612fc897533e932c1a2c4c04dd2e78feb13241e
Srinivas Surakanti <srinivas@Srinivass-Mini.lan>
2026-08-30 17:33:02 -0400

    Deploy updated Surakanti homepage

---
+++ scripts/build-main-site.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+set -eu
+
+mkdir -p .dist-main/ventures/assets
+cp index.html .dist-main/index.html
+cp ventures/assets/hero-village-enterprise.jpg .dist-main/ventures/assets/hero-village-enterprise.jpg
